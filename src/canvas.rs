6bf8719da7c77461ddc43091b11db39c7c623e1d
Melker Widén <melker.widen@gmail.com>
2023-03-26 01:06:25 +0100

    Updated documentation.

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,4 +1,4 @@
-/// Canvas object.
+/// Canvas struct.
 pub struct Canvas {
     width: usize,
     height: usize,
@@ -7,7 +7,7 @@ pub struct Canvas {
 }
 
 impl Canvas {
-    /// Create a new Canvas object with specified width, height and charset.
+    /// Creates a new Canvas instance with specified width, height and charset.
     pub fn new(width: usize, height: usize, charset: &'static [u8]) -> Self {
         Self {
             width,
@@ -17,22 +17,22 @@ impl Canvas {
         }
     }
 
-    /// Get width of Canvas.
+    /// Gets width of Canvas.
     pub fn width(&self) -> usize {
         self.width
     }
 
-    /// Get height of Canvas.
+    /// Gets height of Canvas.
     pub fn height(&self) -> usize {
         self.height
     }
 
-    /// Get brightness value of pixel at (x, y).
+    /// Gets brightness value of pixel at (x, y).
     pub fn get_pixel(&self, x: f64, y: f64) -> f64 {
         self.pixels[self.coordinate_to_pixel_index(x, y)]
     }
 
-    /// Set brightness value of pixel at (x, y).
+    /// Sets brightness value of pixel at (x, y).
     pub fn set_pixel(&mut self, x: f64, y: f64, brightness: f64) {
         if brightness < 0.0f64 || brightness > 1.0f64 {
             panic!("0.0 <= brightness <= 1.0")
@@ -42,32 +42,31 @@ impl Canvas {
         self.pixels[pixel_index] = brightness;
     }
 
-    /// Apply a function to all pixels.
+    /// Applies a function to all pixels.
     pub fn map_pixels(&mut self, func: impl Fn(f64) -> f64) {
         for pixel_value in self.pixels.iter_mut() {
             *pixel_value = func(*pixel_value);
         }
     }
 
-    /// Resize canvas.
+    /// Resizes canvas.
     pub fn resize(mut self, width: usize, height: usize) {
         self.width = width;
         self.height = height;
         self.pixels.resize(width * height, 0.0f64);
     }
 
-    /// Render canvas to stdout.
+    /// Renders the canvas to stdout.
     pub fn render(&self) {
         for (i, pixel_value) in self.pixels.iter().enumerate() {
             print!("{}  ", self.brightness_to_char(*pixel_value));
-
             if (i + 1) % self.width == 0 {
                 println!()
             }
         }
     }
 
-    /// Check if a coordinate (x, y) is within Canvas bounds.
+    /// Checks if a coordinate (x, y) is within Canvas bounds.
     pub fn in_canvas(&self, x: f64, y: f64) -> bool {
         0.0f64 <= x && x < self.width as f64 && 0.0f64 <= y && y < self.height as f64
     }
