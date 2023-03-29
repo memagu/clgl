374deb9fbdb38b8bdd4648cdb82ba015691c0796
Melker Widén <melker.widen@gmail.com>
2023-03-29 19:17:09 +0200

    Optimized canvas::Canvas::render and changed canvas::Canvas::brightness_to_char to canvas::Canvas::brightness_to_char_as_byte where the return type changed from char to u8.

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,3 +1,5 @@
+use std::io::{self, Write, BufWriter};
+
 /// Canvas struct.
 pub struct Canvas {
     width: usize,
@@ -32,14 +34,10 @@ impl Canvas {
         self.pixels[self.coordinate_to_pixel_index(x, y)]
     }
 
-    /// Sets brightness value of pixel at (x, y).
+    /// Sets brightness value of pixel at (x, y). The brightness value is clamped between 0 and 1.
     pub fn set_pixel(&mut self, x: f64, y: f64, brightness: f64) {
-        if brightness < 0.0f64 || brightness > 1.0f64 {
-            panic!("0.0 <= brightness <= 1.0")
-        }
-
         let pixel_index: usize = self.coordinate_to_pixel_index(x, y);
-        self.pixels[pixel_index] = brightness;
+        self.pixels[pixel_index] = brightness.clamp(0.0f64, 1.0f64);
     }
 
     /// Applies a function to all pixels.
@@ -58,14 +56,17 @@ impl Canvas {
 
     /// Renders the canvas to stdout.
     pub fn render(&self) {
-        let mut render_string = String::new();
+        let mut buffer: Vec<u8> = Vec::with_capacity((self.width + 1) * self.height);
+        let mut writer: io::Stdout = io::stdout();
+
         for (i, pixel_value) in self.pixels.iter().enumerate() {
-            render_string.push(self.brightness_to_char(*pixel_value));
+            buffer.push(self.brightness_to_char_as_byte(*pixel_value));
             if (i + 1) % self.width == 0 {
-                render_string.push('\n');
+                buffer.push(b'\n');
             }
         }
-        print!("{}", render_string);
+        writer.write_all(&buffer).unwrap();
+        writer.flush().unwrap();
     }
 
     /// Checks if a coordinate (x, y) is within Canvas bounds.
@@ -73,10 +74,10 @@ impl Canvas {
         0.0f64 <= x && x < self.width as f64 && 0.0f64 <= y && y < self.height as f64
     }
 
-    /// Returns character corresponding to a specified brightness value.
-    fn brightness_to_char(&self, brightness: f64) -> char {
+    /// Returns the character corresponding to a specified brightness value as bytes.
+    fn brightness_to_char_as_byte(&self, brightness: f64) -> u8 {
         let char_index: usize = (brightness * ((self.charset.len() - 1) as f64)) as usize;
-        self.charset[char_index] as char
+        self.charset[char_index]
     }
 
     /// Flattens a coordinate (x, y) to a index.
